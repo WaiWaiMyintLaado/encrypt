425305513e48533204b7066371912173b167937a
WaiWaiMyintMIT <wwm@mit.com.mm>
2021-05-19 22:59:53 +0630

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, Injectable } from '@angular/core';
+import * as CryptoJS from 'node_modules/crypto-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'encrypt';
+  msg = '';
+  result = '';
+  showResult = false;
+  encryptData(){
+    this.result = CryptoJS.AES.encrypt(JSON.stringify(this.msg), 'secret').toString();
+    this.showResult = true;
+  }
+  decryptData(){
+    this.result = JSON.parse(CryptoJS.AES.decrypt(this.msg, 'secret').toString(CryptoJS.enc.Utf8));
+    this.showResult = true;
+  }
 }
